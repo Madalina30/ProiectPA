83a7ec7813b51d7c4f280eb21dee02a9dce74802
LiviuC <62299917+LiviuC-glitch@users.noreply.github.com>
2021-05-19 14:57:22 +0300

    rest almost

--- app/src/main/java/com/example/exam_project/ExamSyncAdapter.java
+++ app/src/main/java/com/example/exam_project/ExamSyncAdapter.java
@@ -1,22 +1,38 @@
 package com.example.exam_project;
 
 import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.SyncRequest;
 import android.content.SyncResult;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class ExamSyncAdapter extends AbstractThreadedSyncAdapter {
 
     public final String LOG_TAG = ExamSyncAdapter.class.getSimpleName();
     public static final int SYNC_INTERVAL = 60 * 180;
-    public static final int SYNC_FLEXTIME = SYNC_INTERVAL/3;
+    public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
     ContentResolver mContentResolver;
 
     public ExamSyncAdapter(Context context, boolean autoInitialize) {
@@ -31,6 +47,155 @@ public class ExamSyncAdapter extends AbstractThreadedSyncAdapter {
 
         HttpURLConnection urlConnection = null;
         String examJsonStr = null;
+        try {
+            final String EXAM_URL = "https://exam-net.herokuapp.com/exams/examcontroller.php?view=all";
+
+            Uri builtUri = Uri.parse(EXAM_URL).buildUpon().build();
+
+            URL url = new URL(builtUri.toString());
+            Log.d(LOG_TAG, "The URL link is  " + url);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(1000000 /* milliseconds */);
+            urlConnection.setConnectTimeout(1500000/* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            examJsonStr = buffer.toString();
+            getExamData(examJsonStr);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error passing data ", e);
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+        }
+        return;
+    }
+
+    private void getExamData(String examDataJson) throws JSONException {
+        final String USERS = "exams";
+        final String USERNAME = "username";
+        final String PASS = "password";
+        final String EMAIL = "email";
+        final String POINTS = "punctaj";
+
+        try {
+            JSONObject examJson = new JSONObject(examDataJson);
+            JSONArray examArray = examJson.getJSONArray(USERS);
+
+            for (int i = 0; i < examArray.length(); i++) {
+                String username, password, email, points;
+                JSONObject examDetails = examArray.getJSONObject(i);
+
+                username = examDetails.getString(USERNAME);
+                password = examDetails.getString(PASS);
+                email = examDetails.getString(PASS);
+                points = examDetails.getString(POINTS);
+
+                ContentValues examValues = new ContentValues();
+
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+            e.printStackTrace();
+        }
 
     }
+
+    public static void configurePeriodicSync(Context context, int syncInterval, int flexTime) {
+        Account account = getSyncAccount(context);
+        String authority = context.getString(R.string.content_authority);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // we can enable inexact timers in our periodic sync
+            SyncRequest request = new SyncRequest.Builder().
+                    syncPeriodic(syncInterval, flexTime).
+                    setSyncAdapter(account, authority).
+                    setExtras(new Bundle()).build();
+            ContentResolver.requestSync(request);
+        } else {
+            ContentResolver.addPeriodicSync(account,
+                    authority, new Bundle(), syncInterval);
+        }
+    }
+    public static void syncImmediately(Context context) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+        ContentResolver.requestSync(getSyncAccount(context),
+                context.getString(R.string.content_authority), bundle);
+    }
+
+    public static Account getSyncAccount(Context context) {
+        // Get an instance of the Android account manager
+        AccountManager accountManager = (AccountManager) context.getSystemService(Context.ACCOUNT_SERVICE);
+
+        // Create the account type and default account
+        Account newAccount = new Account(context.getString(R.string.app_name), context.getString(R.string.sync_account_type));
+
+        // If the password doesn't exist, the account doesn't exist
+        if (null == accountManager.getPassword(newAccount)) {
+
+            /*
+             * Add the account and account type, no password or user data
+             * If successful, return the Account object, otherwise report an error.
+             */
+            if (!accountManager.addAccountExplicitly(newAccount, "", null)) {
+                return null;
+            }
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call ContentResolver.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+
+            onAccountCreated(newAccount, context);
+        }
+        return newAccount;
+    }
+
+    private static void onAccountCreated(Account newAccount, Context context) {
+
+        ExamSyncAdapter.configurePeriodicSync(context, SYNC_INTERVAL, SYNC_FLEXTIME);
+        ContentResolver.setSyncAutomatically(newAccount, context.getString(R.string.content_authority), true);
+        syncImmediately(context);
+    }
+
+    public static void initializeSyncAdapter(Context context) {
+        getSyncAccount(context);
+    }
+
 }
